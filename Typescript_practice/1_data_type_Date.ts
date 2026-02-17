774da1770aa693b27bfee43c6dfee17565da007c
Thandavhathu Khumbelo <thandakhumbe@gmail.com>
2026-02-17 15:27:49 +0200

    Add files via upload

---
+++ Typescript_practice/1_data_type_Date.ts
@@ -0,0 +1,4 @@
+// let paymentDate.ts
+let paymentDate: Date = new Date ("2026-04-16")
+
+console.log("your new payment date is: " + paymentDate.toDateString());
---
+++ Typescript_practice/1_data_type_number.ts
@@ -0,0 +1,12 @@
+let interestRate: number =0.25
+let loanAmount: number =28000
+
+// interestAmount.ts
+let interestAmount: number = interestRate*loanAmount;
+console.log(interestRate*loanAmount);
+
+// totalPayment.ts
+let totalPayment: number = interestAmount + loanAmount;
+console.log(interestAmount + loanAmount);
+
+ 
